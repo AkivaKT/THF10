c9fd8d14fa53d737f2b46482c43eff673ec8aa46
hyeonbeom lee <hyeonbeom920723@gmail.com>
2019-07-02 16:11:11 -0600

    Pull data

---
+++ app/src/main/java/com/byui/thf10/ProductActivity.java
@@ -0,0 +1,18 @@
+package com.byui.thf10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+        setTitle("Product Activity");
+    }
+
+
+}
