6bc8f727c1f70a870398bedf0d8d6d8eb321183e
Your Name <sy17001@byui.edu>
2019-07-12 18:35:01 -0600

    sales work

--- app/src/main/java/com/byui/thf10/SalesActivity.java
+++ app/src/main/java/com/byui/thf10/SalesActivity.java
@@ -3,6 +3,7 @@ package com.byui.thf10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,18 +21,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static android.R.layout.simple_spinner_item;
+import static android.content.ContentValues.TAG;
 
 public class SalesActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private EditText Price;
+    private EditText Product;
     private EditText Quantity;
     private FireStore firedb;
     private ArrayList<JsonConvertible> salesList = new ArrayList<>();
+    private Button saveButton;
+    private Button sendButton;
+    private Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales);
 
+        // Product Spinner
         Product spinner;
         Spinner ProductSpinner = findViewById(R.id.Product);
         ProductSpinner.setOnItemSelectedListener(this);
@@ -51,6 +59,7 @@ public class SalesActivity extends AppCompatActivity implements AdapterView.OnIt
         ProductSpinner.setAdapter(ProductAdapter);
         ProductSpinner.setOnItemSelectedListener(this);
 
+        // Price Spinner
         Price Spinner;
         Spinner PriceSpinner = findViewById(R.id.Price);
         PriceSpinner.setOnItemSelectedListener(this);
@@ -69,17 +78,48 @@ public class SalesActivity extends AppCompatActivity implements AdapterView.OnIt
         PriceSpinner.setAdapter(PriceAdapter);
         PriceSpinner.setOnItemSelectedListener(this);
 
-        // Quantity Value
-        //Quantity = (EditText) findViewById(R.id.Quantity);
+        // Quantity Entry box
+        Quantity = findViewById(R.id.Quantity);
 
-        // Json Convertible
-        Type listType = new TypeToken<List<JsonConvertible>>() {
-        }.getType();
-        List<JsonConvertible> SaleEntry = new LinkedList<>();
-        //SaleEntry.add(ProductSpinner);
-        //SaleEntry.add(PriceSpinner);
-        //SaleEntry.add(Quantity);
+        saveButton = findViewById(R.id.SaveButton);
+        sendButton = findViewById(R.id.SendButton);
+        deleteButton = findViewById(R.id.deleteButton);
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveInfo();
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              sendInfo();
+                                          }
+                                      }
+        );
+
+
+    }
+
+    public void saveInfo() {
+        String getQuantity = Quantity.getText().toString();
+        //String getPrice = Price.getText().toString();
+        //String getProduct = Product.getText().toString();
+
+        Sale sale = new Sale();
+
+        if (getQuantity == null || getQuantity.trim().equals("")){ //|| getPrice == null || getPrice.trim().equals("") || getProduct == null || getProduct.trim().equals(""))  {
+            Toast.makeText(getBaseContext(), "Input field is empty", Toast.LENGTH_LONG).show();
+        }
+        else {
+            sale.setQuantity(getQuantity);
+            //sale.setPrice(getPrice);
+            //sale.setProduct(getProduct);
+            salesList.add(sale);
+            Log.i(TAG, "sdd Sale created.");
+        }
     }
 
     @Override
@@ -99,4 +139,5 @@ public class SalesActivity extends AppCompatActivity implements AdapterView.OnIt
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
