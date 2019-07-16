3ad63e50c712a5122e0581cfe7e68fe81e279e63
AkivaKT <tun18001@byui.edu>
2019-07-15 19:22:24 -0600

    price documentation finished

--- app/src/main/java/com/byui/thf10/Price.java
+++ app/src/main/java/com/byui/thf10/Price.java
@@ -7,11 +7,9 @@ public class Price extends JsonConvertible{
 
     //member variables
     private String start_date;
-
     private String end_date;
     private float amount;
     private boolean active;
-
     private String description;
 
     // getters
@@ -26,20 +24,35 @@ public class Price extends JsonConvertible{
 
     public String getDescription() { return description; }
 
-
     // setters
 
-
+    /**
+     * making price available
+     * @param active if the price is available at this point or not
+     */
     public void setActive(boolean active) { this.active = active;}
 
+    /**
+     * setting description of the price
+     * @param description the simple description of the price (e.g. 10% off, regular)
+     */
     public void setDescription(String description) { this.description = description; }
 
     public void setAmount(float amount) {
         this.amount = amount;
     }
 
+    /**
+     * Nullable end_date, built for future reference. Price expires on the end date, and it will
+     * no longer be active. (yet to implement)
+     * @param end_date
+     */
     public void setEnd_date(@Nullable String end_date) { this.end_date = end_date;}
 
+    /**
+     * Start date , the price becomes active on the start date.
+     * @param start_date
+     */
     public void setStart_date(String start_date) {
         this.start_date = start_date;
     }
